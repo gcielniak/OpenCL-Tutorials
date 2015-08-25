6fbc3f846a7ab07d4dbd9aae3289bfd092f4d1c6
gcielniak <gcielniak@lincoln.ac.uk>
2015-08-25 16:47:13 +0100

    C++ bindings version

--- src/Tutorial1.cpp
+++ src/Tutorial1.cpp
@@ -1,109 +1,194 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#ifdef __APPLE__
-    #include "OpenCL/opencl.h"
+
+#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
+#define __CL_ENABLE_EXCEPTIONS
+
+#if defined(__APPLE__) || defined(__MACOSX)
+#include <OpenCL/cl.hpp>
 #else
-    #include "CL/cl.h"
+#include <CL/cl.hpp>
 #endif
 
-std::string GetPlatformName (cl_platform_id id)
+std::string GetPlatformName(cl_platform_id id)
 {
 	size_t size = 0;
-	clGetPlatformInfo (id, CL_PLATFORM_NAME, 0, nullptr, &size);
+	clGetPlatformInfo(id, CL_PLATFORM_NAME, 0, nullptr, &size);
 
 	std::string result;
-	result.resize (size);
-	clGetPlatformInfo (id, CL_PLATFORM_NAME, size,
-		const_cast<char*> (result.data ()), nullptr);
+	result.resize(size);
+	clGetPlatformInfo(id, CL_PLATFORM_NAME, size,
+		const_cast<char*> (result.data()), nullptr);
 
 	return result;
 }
 
-std::string GetDeviceName (cl_device_id id)
+std::string GetDeviceName(cl_device_id id)
 {
 	size_t size = 0;
-	clGetDeviceInfo (id, CL_DEVICE_NAME, 0, nullptr, &size);
+	clGetDeviceInfo(id, CL_DEVICE_NAME, 0, nullptr, &size);
 
 	std::string result;
-	result.resize (size);
-	clGetDeviceInfo (id, CL_DEVICE_NAME, size,
-		const_cast<char*> (result.data ()), nullptr);
+	result.resize(size);
+	clGetDeviceInfo(id, CL_DEVICE_NAME, size,
+		const_cast<char*> (result.data()), nullptr);
 
 	return result;
+}
+
+const char *getErrorString(cl_int error)
+{
+	switch (error){
+		// run-time and JIT compiler errors
+	case 0: return "CL_SUCCESS";
+	case -1: return "CL_DEVICE_NOT_FOUND";
+	case -2: return "CL_DEVICE_NOT_AVAILABLE";
+	case -3: return "CL_COMPILER_NOT_AVAILABLE";
+	case -4: return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+	case -5: return "CL_OUT_OF_RESOURCES";
+	case -6: return "CL_OUT_OF_HOST_MEMORY";
+	case -7: return "CL_PROFILING_INFO_NOT_AVAILABLE";
+	case -8: return "CL_MEM_COPY_OVERLAP";
+	case -9: return "CL_IMAGE_FORMAT_MISMATCH";
+	case -10: return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
+	case -11: return "CL_BUILD_PROGRAM_FAILURE";
+	case -12: return "CL_MAP_FAILURE";
+	case -13: return "CL_MISALIGNED_SUB_BUFFER_OFFSET";
+	case -14: return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
+	case -15: return "CL_COMPILE_PROGRAM_FAILURE";
+	case -16: return "CL_LINKER_NOT_AVAILABLE";
+	case -17: return "CL_LINK_PROGRAM_FAILURE";
+	case -18: return "CL_DEVICE_PARTITION_FAILED";
+	case -19: return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
+
+		// compile-time errors
+	case -30: return "CL_INVALID_VALUE";
+	case -31: return "CL_INVALID_DEVICE_TYPE";
+	case -32: return "CL_INVALID_PLATFORM";
+	case -33: return "CL_INVALID_DEVICE";
+	case -34: return "CL_INVALID_CONTEXT";
+	case -35: return "CL_INVALID_QUEUE_PROPERTIES";
+	case -36: return "CL_INVALID_COMMAND_QUEUE";
+	case -37: return "CL_INVALID_HOST_PTR";
+	case -38: return "CL_INVALID_MEM_OBJECT";
+	case -39: return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
+	case -40: return "CL_INVALID_IMAGE_SIZE";
+	case -41: return "CL_INVALID_SAMPLER";
+	case -42: return "CL_INVALID_BINARY";
+	case -43: return "CL_INVALID_BUILD_OPTIONS";
+	case -44: return "CL_INVALID_PROGRAM";
+	case -45: return "CL_INVALID_PROGRAM_EXECUTABLE";
+	case -46: return "CL_INVALID_KERNEL_NAME";
+	case -47: return "CL_INVALID_KERNEL_DEFINITION";
+	case -48: return "CL_INVALID_KERNEL";
+	case -49: return "CL_INVALID_ARG_INDEX";
+	case -50: return "CL_INVALID_ARG_VALUE";
+	case -51: return "CL_INVALID_ARG_SIZE";
+	case -52: return "CL_INVALID_KERNEL_ARGS";
+	case -53: return "CL_INVALID_WORK_DIMENSION";
+	case -54: return "CL_INVALID_WORK_GROUP_SIZE";
+	case -55: return "CL_INVALID_WORK_ITEM_SIZE";
+	case -56: return "CL_INVALID_GLOBAL_OFFSET";
+	case -57: return "CL_INVALID_EVENT_WAIT_LIST";
+	case -58: return "CL_INVALID_EVENT";
+	case -59: return "CL_INVALID_OPERATION";
+	case -60: return "CL_INVALID_GL_OBJECT";
+	case -61: return "CL_INVALID_BUFFER_SIZE";
+	case -62: return "CL_INVALID_MIP_LEVEL";
+	case -63: return "CL_INVALID_GLOBAL_WORK_SIZE";
+	case -64: return "CL_INVALID_PROPERTY";
+	case -65: return "CL_INVALID_IMAGE_DESCRIPTOR";
+	case -66: return "CL_INVALID_COMPILER_OPTIONS";
+	case -67: return "CL_INVALID_LINKER_OPTIONS";
+	case -68: return "CL_INVALID_DEVICE_PARTITION_COUNT";
+
+		// extension errors
+	case -1000: return "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
+	case -1001: return "CL_PLATFORM_NOT_FOUND_KHR";
+	case -1002: return "CL_INVALID_D3D10_DEVICE_KHR";
+	case -1003: return "CL_INVALID_D3D10_RESOURCE_KHR";
+	case -1004: return "CL_D3D10_RESOURCE_ALREADY_ACQUIRED_KHR";
+	case -1005: return "CL_D3D10_RESOURCE_NOT_ACQUIRED_KHR";
+	default: return "Unknown OpenCL error";
+	}
 }
 
 void CheckError(cl_int error)
 {
 	if (error != CL_SUCCESS) {
-		std::cerr << "OpenCL call failed with error " << error << std::endl;
-		std::exit (1);
+		std::cerr << "OpenCL call failed with error " << getErrorString(error) << std::endl;
+		std::exit(1);
 	}
 }
 
+using namespace std;
+
 int main()
 {
-	cl_uint platformIdCount = 0;
-	cl_uint deviceIdCount = 0;
-	cl_int platform = 0;
-	cl_int device = 0;
+	vector<cl::Platform> platforms;
 
-	clGetPlatformIDs (0, nullptr, &platformIdCount);
-
-	std::cout << "Found " << platformIdCount << " platform(s)" << std::endl;
-
-	std::vector<cl_platform_id> platformIds (platformIdCount);
-	clGetPlatformIDs (platformIdCount, platformIds.data (), nullptr);
-	if (platformIdCount)
-		platform = 0;
-
-	for (cl_uint i = 0; i < platformIdCount; ++i) 
+	try
 	{
-		std::cout << "\t (" << (i+1) << ") : " << GetPlatformName (platformIds [i]) << std::endl;
+		cl::Platform::get(&platforms);
 
-		clGetDeviceIDs (platformIds[i], CL_DEVICE_TYPE_ALL, 0, nullptr, &deviceIdCount);
-		std::cout << "\t\tFound " << deviceIdCount << " device(s)" << std::endl;
+		cout << "Found " << platforms.size() << " platform(s):" << endl;
 
-		std::vector<cl_device_id> deviceIds (deviceIdCount);
-		clGetDeviceIDs (platformIds[i], CL_DEVICE_TYPE_ALL, deviceIdCount,	deviceIds.data (), nullptr);
+		for (unsigned int i = 0; i < platforms.size(); i++)
+		{
+			string platform_name;
+			platforms[i].getInfo((cl_platform_info)CL_PLATFORM_NAME, &platform_name);
+			string platform_vendor;
+			platforms[i].getInfo((cl_platform_info)CL_PLATFORM_VENDOR, &platform_vendor);
 
-		if 	(platform == i)
-			device = 0;
+			cout << " " << (i + 1) << ") " << platform_name << " [" << platform_vendor << "]" << endl;
 
-		for (cl_uint j = 0; j < deviceIdCount; ++j)
-			std::cout << "\t\t (" << (j+1) << ") : " << GetDeviceName (deviceIds[j]) << std::endl;
+			vector<cl::Device> devices;
+
+			platforms[i].getDevices((cl_device_type)CL_DEVICE_TYPE_ALL, &devices);
+
+			cout << "\tFound " << devices.size() << " device(s):" << endl;
+
+			for (unsigned int j = 0; j < devices.size(); j++)
+			{
+				string device_name;
+				devices[j].getInfo((cl_device_info)CL_DEVICE_NAME, &device_name);
+
+				cl_device_type device_type;
+				devices[j].getInfo((cl_device_info)CL_DEVICE_TYPE, &device_type);
+
+				cout << "\t " << (j + 1) << ") " << device_name << endl;
+			}
+		}
+	}
+	catch (cl::Error err) {
+		cerr << "ERROR: " << err.what() << "(" << err.err() << ")" << endl;
 	}
 
-	if ((platform < 0) || (device < 0))
-	{
-		std::cerr << "No compatible OpenCL device detected." << std::endl;
-		return 0;
-	}
-
-	const cl_context_properties contextProperties [] =
+	/*
+	const cl_context_properties contextProperties[] =
 	{
 		CL_CONTEXT_PLATFORM,
 		reinterpret_cast<cl_context_properties> (platformIds[platform]),
 		0, 0
 	};
-	
-	cl_int error;
 
-	clGetDeviceIDs (platformIds[platform], CL_DEVICE_TYPE_ALL, 0, nullptr, &deviceIdCount);
-	std::vector<cl_device_id> deviceIds (deviceIdCount);
-	clGetDeviceIDs (platformIds[platform], CL_DEVICE_TYPE_ALL, deviceIdCount,	deviceIds.data (), nullptr);
+	clGetDeviceIDs(platformIds[platform], CL_DEVICE_TYPE_ALL, 0, nullptr, &deviceIdCount);
+	std::vector<cl_device_id> deviceIds(deviceIdCount);
+	clGetDeviceIDs(platformIds[platform], CL_DEVICE_TYPE_ALL, deviceIdCount, deviceIds.data(), nullptr);
 
-	cl_context context = clCreateContext(contextProperties, deviceIdCount,	deviceIds.data (), nullptr,
+	cl_context context = clCreateContext(contextProperties, deviceIdCount, deviceIds.data(), nullptr,
 		nullptr, &error);
 
 	CheckError(error);
 
-	cl_command_queue queue = clCreateCommandQueue (context, deviceIds[device], 0, &error);
-	
-	CheckError (error);
-	
-	clReleaseCommandQueue (queue);
-	clReleaseContext (context);
+	cl_command_queue queue = clCreateCommandQueue(context, deviceIds[device], 0, &error);
 
+	CheckError(error);
+
+	clReleaseCommandQueue(queue);
+	clReleaseContext(context);
+
+	*/
 	return 0;
 }
