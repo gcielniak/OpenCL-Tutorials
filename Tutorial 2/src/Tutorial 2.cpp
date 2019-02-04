fd28ebd1179113b1a5d5d26e49c8858f8dc5a6cd
gcielniak <gcielniak@gmail.com>
2019-02-04 12:22:22 +0000

    Tutorial 2 working

--- Tutorial 2/src/Tutorial 2.cpp
+++ Tutorial 2/src/Tutorial 2.cpp
@@ -21,6 +21,7 @@ void print_help() {
 	std::cerr << "  -p : select platform " << std::endl;
 	std::cerr << "  -d : select device" << std::endl;
 	std::cerr << "  -l : list all platforms and devices" << std::endl;
+	std::cerr << "  -f : input image file (default: test.ppm)" << std::endl;
 	std::cerr << "  -h : print this message" << std::endl;
 }
 
@@ -28,17 +29,21 @@ int main(int argc, char **argv) {
 	//Part 1 - handle command line options such as device selection, verbosity, etc.
 	int platform_id = 0;
 	int device_id = 0;
+	string image_filename = "test.ppm";
 
 	for (int i = 1; i < argc; i++) {
 		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
 		else if ((strcmp(argv[i], "-d") == 0) && (i < (argc - 1))) { device_id = atoi(argv[++i]); }
 		else if (strcmp(argv[i], "-l") == 0) { std::cout << ListPlatformsDevices() << std::endl; }
-		else if (strcmp(argv[i], "-h") == 0) { print_help(); }
+		else if ((strcmp(argv[i], "-f") == 0) && (i < (argc - 1))) { image_filename = argv[++i]; }
+		else if (strcmp(argv[i], "-h") == 0) { print_help(); return 0; }
 	}
+
+	cimg::exception_mode(0);
 
 	//detect any potential exceptions
 	try {
-		CImg<unsigned char> image_input("test.ppm");
+		CImg<unsigned char> image_input(image_filename.c_str());
 		CImgDisplay disp_input(image_input,"input");
 
 		//a 3x3 convolution mask implementing an averaging filter
@@ -79,35 +84,39 @@ int main(int argc, char **argv) {
 		//device - buffers
 		cl::Buffer dev_image_input(context, CL_MEM_READ_ONLY, image_input.size());
 		cl::Buffer dev_image_output(context, CL_MEM_READ_WRITE, image_input.size()); //should be the same as input image
-		//cl::Buffer dev_convolution_mask(context, CL_MEM_READ_ONLY, convolution_mask.size()*sizeof(float));
+//		cl::Buffer dev_convolution_mask(context, CL_MEM_READ_ONLY, convolution_mask.size()*sizeof(float));
 
 		//4.1 Copy images to device memory
 		queue.enqueueWriteBuffer(dev_image_input, CL_TRUE, 0, image_input.size(), &image_input.data()[0]);
-		//queue.enqueueWriteBuffer(dev_convolution_mask, CL_TRUE, 0, convolution_mask.size()*sizeof(float), &convolution_mask[0]);
+//		queue.enqueueWriteBuffer(dev_convolution_mask, CL_TRUE, 0, convolution_mask.size()*sizeof(float), &convolution_mask[0]);
 
 		//4.2 Setup and execute the kernel (i.e. device code)
-		cl::Kernel kernel = cl::Kernel(program, "identity");
+		cl::Kernel kernel = cl::Kernel(program, "identityND");
 		kernel.setArg(0, dev_image_input);
 		kernel.setArg(1, dev_image_output);
-		//kernel.setArg(2, dev_convolution_mask);
+//		kernel.setArg(2, dev_convolution_mask);
 
-		queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(image_input.width()*image_input.height()), cl::NullRange);
+		queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(image_input.width(), image_input.height(), image_input.spectrum()), cl::NullRange);
 
 		vector<unsigned char> output_buffer(image_input.size());
 		//4.3 Copy the result from device to host
 		queue.enqueueReadBuffer(dev_image_output, CL_TRUE, 0, output_buffer.size(), &output_buffer.data()[0]);
 
-		CImg<unsigned char> output_image(output_buffer.data(), output_buffer.width(), output_buffer.height(), output_buffer.depth(), output_buffer.spectrum());
+		CImg<unsigned char> output_image(output_buffer.data(), image_input.width(), image_input.height(), image_input.depth(), image_input.spectrum());
 		CImgDisplay disp_output(output_image,"output");
 
- 		while (!disp_input.is_closed() && !disp_output.is_closed()) {
-		    disp_input.wait();
-		    disp_output.wait();
+ 		while (!disp_input.is_closed() && !disp_output.is_closed()
+			&& !disp_input.is_keyESC() && !disp_output.is_keyESC()) {
+		    disp_input.wait(1);
+		    disp_output.wait(1);
 	    }		
 
 	}
 	catch (const cl::Error& err) {
 		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
+	}
+	catch (CImgException& err) {
+		std::cerr << "ERROR: " << err.what() << std::endl;
 	}
 
 	return 0;
--- Tutorial 2/src/kernels/my_kernels_2.cl
+++ Tutorial 2/src/kernels/my_kernels_2.cl
@@ -1,48 +1,75 @@
 //a simple OpenCL kernel which copies all pixels from A to B
-kernel void identity(global const uchar4* A, global uchar4* B) {
+kernel void identity(global const uchar* A, global uchar* B) {
 	int id = get_global_id(0);
 	B[id] = A[id];
 }
 
-//simple 2D identity kernel
-kernel void identity2D(global const uchar4* A, global uchar4* B) {
-	int x = get_global_id(0);
-	int y = get_global_id(1);
-	int width = get_global_size(0); //width in pixels
-	int id = x + y*width;
+kernel void filter_r(global const uchar* A, global uchar* B) {
+	int id = get_global_id(0);
+	int image_size = get_global_size(0)/3; //each image consists of 3 colour channels
+	int colour_channel = id / image_size; // 0 - red, 1 - green, 2 - blue
+
+	B[id] = A[id];
+}
+
+//simple ND identity kernel
+kernel void identityND(global const uchar* A, global uchar* B) {
+	int width = get_global_size(0); //image width in pixels
+	int height = get_global_size(1); //image height in pixels
+	int image_size = width*height; //image size in pixels
+	int channels = get_global_size(2); //number of colour channels: 3 for RGB
+
+	int x = get_global_id(0); //current x coord.
+	int y = get_global_id(1); //current y coord.
+	int c = get_global_id(2); //current colour channel
+
+	int id = x + y*width + c*image_size; //global id in 1D space
+
 	B[id] = A[id];
 }
 
 //2D averaging filter
-kernel void avg_filter2D(global const uchar4* A, global uchar4* B) {
-	int x = get_global_id(0);
-	int y = get_global_id(1);
-	int width = get_global_size(0); //width in pixels
-	int id = x + y*width;
+kernel void avg_filter2D(global const uchar* A, global uchar* B) {
+	int width = get_global_size(0); //image width in pixels
+	int height = get_global_size(1); //image height in pixels
+	int image_size = width*height; //image size in pixels
+	int channels = get_global_size(2); //number of colour channels: 3 for RGB
 
-	uint4 result = (uint4)(0);//zero all 4 components
+	int x = get_global_id(0); //current x coord.
+	int y = get_global_id(1); //current y coord.
+	int c = get_global_id(2); //current colour channel
+
+	int id = x + y*width + c*image_size; //global id in 1D space
+
+	ushort result = 0;
 
 	for (int i = (x-1); i <= (x+1); i++)
 	for (int j = (y-1); j <= (y+1); j++) 
-		result += convert_uint4(A[i + j*width]); //convert pixel values to uint4 so the sum can be larger than 255
+		result += A[i + j*width + c*image_size];
 
-	result /= (uint4)(9); //normalise all components (the result is a sum of 9 values) 
+	result /= 9;
 
-	B[id] = convert_uchar4(result); //convert back to uchar4 
+	B[id] = (uchar)result;
 }
 
 //2D 3x3 convolution kernel
-kernel void convolution2D(global const uchar4* A, global uchar4* B, constant float* mask) {
-	int x = get_global_id(0);
-	int y = get_global_id(1);
-	int width = get_global_size(0); //width in pixels
-	int id = x + y*width;
+kernel void convolution2D(global const uchar* A, global uchar* B, constant float* mask) {
+	int width = get_global_size(0); //image width in pixels
+	int height = get_global_size(1); //image height in pixels
+	int image_size = width*height; //image size in pixels
+	int channels = get_global_size(2); //number of colour channels: 3 for RGB
 
-	float4 result = (float4)(0.0f,0.0f,0.0f,0.0f);//zero all 4 components
+	int x = get_global_id(0); //current x coord.
+	int y = get_global_id(1); //current y coord.
+	int c = get_global_id(2); //current colour channel
+
+	int id = x + y*width + c*image_size; //global id in 1D space
+
+	ushort result = 0;
 
 	for (int i = (x-1); i <= (x+1); i++)
-	for (int j = (y-1); j <= (y+1); j++)
-		result += convert_float4(A[i + j*width])*(float4)(mask[i-(x-1) + (j-(y-1))*3]);//convert pixel and mask values to float4
+	for (int j = (y-1); j <= (y+1); j++) 
+		result += A[i + j*width + c*image_size]*mask[i-(x-1) + j-(y-1)];
 
-	B[id] = convert_uchar4(result); //convert back to uchar4
+	B[id] = (uchar)result;
 }
