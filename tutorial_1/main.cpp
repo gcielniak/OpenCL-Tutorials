220574811d42c56154d8f7adc1d04037474a62f6
DreamingRaven <GeorgeRavenNR@outlook.com>
2020-01-21 12:15:55 +0000

    Changed name and minor compiler guard

--- tutorial_1/main.cpp
+++ tutorial_1/main.cpp
@@ -1,9 +1,18 @@
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#define __CL_ENABLE_EXCEPTIONS
+
 #include <iostream>
 #include <vector>
-#include <CL/cl.hpp>
+
+// Include guarded openCL libs
+#ifdef __APPLE__
+	#include <OpenCL/cl.hpp>
+#else
+	#include <CL/cl.hpp>
+#endif
 
 int main(int argc, char **argv) {
 
-	std::cout << "Hello someone, SAVE ME!" << std::endl;
+	std::cout << "Hello" << std::endl;
 	return 0;
 }
